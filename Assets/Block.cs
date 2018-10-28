68ba5f14f6bceb962e00e53d612b29b10cfaf587
Kshitij Gajapure <kshitijiitgn@gmail.com>
2018-10-28 16:42:36 +0530

    Added Unity files

---
+++ Assets/Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	void Start()
+	{
+		GetComponent<Rigidbody2D> ().gravityScale += Time.timeSinceLevelLoad / 30f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -2f) {
+			Destroy (gameObject);
+		
+		}
+
+
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public float slowness = 10f;
+
+	public void EndGame()
+	{
+		StartCoroutine (RestartLevel ());
+	}
+
+	IEnumerator RestartLevel()
+	{
+		Time.timeScale = 1f / slowness;
+		Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
+
+		yield return new WaitForSeconds (1f / slowness);
+
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+
+		SceneManager.LoadScene ( SceneManager.GetActiveScene().buildIndex );
+	}
+
+}
